65a3ef5958448e4b38bd0079bf3f96b6bd4a9351
Dennismps <79340987+Dennismps@users.noreply.github.com>
2021-03-22 21:28:02 -0600

    rearange

--- index.js
+++ index.js
@@ -8,37 +8,9 @@ var auto_cps = 0;
 var resturantcps = 0;
 var resturants = 0;
 var resturantcost = 60;
-var n2xcost = 200;
-var n2x = 0;
-
-
-
-
-
-
-
-function buyResturant() {
-  if (bowlcount >= resturantcost) {
-    bowlcount = Math.round(bowlcount - resturantcost);
-    resturants = resturants + 1;
-    resturantcps = Math.round(5.5 * resturants) ;
-    resturantcost = Math.round(resturantcost * 1.5);
-    update();
-  } else {
-    alert("You dont have enough cookies");
-  }
-}
-
-function buyn2x() {
-  if (bowlcount >= n2xcost) {
-    bowlcount = Math.round(bowlcount - n2xcost);
-    n2x = n2x + 1;
-    n2xcost = Math.round(n2xcost * 1.5)
-    update();
-  } else {
-    alert("You dont have enough cookies");
-  }
-}
+var multipliercost = 200;
+var multiplier = 0;
+var multipliervalue = 1;
 
 function update() {
   document.getElementById("text").innerHTML = bowlcount + " Bowls";
@@ -52,19 +24,60 @@ function update() {
   document.getElementById("resturantamount").innerHTML = "Resturants: " + resturants;
   document.getElementById("resturantcps").innerHTML = "Bowls per second: " + resturantcps;
   document.getElementById("resturantcost").innerHTML = "Resturants cost: " + resturantcost;
-  document.getElementById("bps").innerHTML = auto_cps + resturantcps + " bowls per second";
-  document.getElementById("n2xPrice").innerHTML = "2x upgrade costs:" + n2xcost;
-  document.getElementById("n2xAmount").innerHTML = "upgrades:" + n2x;
-  
+  document.getElementById("bps").innerHTML = (auto_cps + resturantcps) * multipliervalue + " bowls per second";
+  document.getElementById("multiplierPrice").innerHTML = "Costs: " + multipliercost;
+  document.getElementById("multiplierAmount").innerHTML = "Amount: " + multiplier;  
 }
 
 function timer() {
-  bowlcount += auto_cps;
-  bowlcount += resturantcps;
+  bowlcount += auto_cps * multipliervalue;
+  bowlcount += resturantcps * multipliervalue;
   update();
 }
 
 setInterval(timer, 1000);
+
+function makeBowl() {
+  bowlcount += bpc;
+  update();
+}
+
+function hireChef() {
+  if (bowlcount >= chefPrice) {
+    bpc = Math.round(bpc + 1.1);
+    chefs += 1;
+    bowlcount = Math.round(bowlcount - chefPrice);
+    chefPrice = Math.round(chefPrice * 1.7);
+    update();
+  } else {
+    alert("You dont have enough cookies");
+  }
+}
+
+
+function buymultiplier() {
+  if (bowlcount >= multipliercost) {
+    bowlcount = Math.round(bowlcount - multipliercost);
+    multiplier += 1;
+    multipliervalue = multiplier * 2;
+    multipliercost = Math.round(multipliercost * 1.5);
+    update();
+  } else {
+    alert("You dont have enough cookies");
+  }
+}
+
+function buyResturant() {
+  if (bowlcount >= resturantcost) {
+    bowlcount = Math.round(bowlcount - resturantcost);
+    resturants = resturants + 1;
+    resturantcps = Math.round((5.5 * resturants) * multipliervalue) ;
+    resturantcost = Math.round(resturantcost * 1.5);
+    update();
+  } else {
+    alert("You dont have enough cookies");
+  }
+}
 
 function buyAutoClicker() {
   if (bowlcount >= autoprice) {
@@ -92,23 +105,6 @@ function load() {
   bowlcount = parseInt(autoclicker);
   bowlcount = localStorage.getItem("resturants");
   bowlcount = parseInt(resturants);
-
   update();
 }
 
-function makeBowl() {
-  bowlcount += bpc;
-  update()
-}
-
-function hireChef() {
-  if (bowlcount >= chefPrice) {
-    bpc = Math.round(bpc + 1.1);
-    chefs += 1;
-    bowlcount = Math.round(bowlcount - chefPrice);
-    chefPrice = Math.round(chefPrice * 1.7);
-    update();
-  } else {
-    alert("You dont have enough cookies");
-  }
-}
---
+++ tooltip.js
@@ -0,0 +1,30 @@
+
+tippy('.click', {
+    content: 'This is a strutre that gives bowls persecond',
+    followCursor: 'vertical',
+    duration: 0,
+    placement: 'left',
+    arrow: false,
+})
+tippy('.chef', {
+    content: 'This is a strutre that gives more bowls per click',
+    followCursor: 'vertical',
+    duration: 0,
+    placement: 'right',
+    arrow: false,
+})
+tippy('.rest', {
+    content: 'This is a strutre that gives bowls persecond',
+    followCursor: 'vertical',
+    duration: 0,
+    placement: 'left',
+    arrow: false,
+})
+
+tippy('.multi', {
+    content: ' this 2xs your resturants and clickers',
+    followCursor: 'horizontal',
+    duration: 0,
+    placement: 'bottom',
+    arrow: false,
+})
